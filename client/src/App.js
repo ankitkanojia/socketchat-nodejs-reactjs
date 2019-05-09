59efc7bb3f2443e462c07adc661d04facade3f11
ankitkaojia <ankit.devdigital@gmail.com>
2019-05-09 14:59:11 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Chat from './pages/chat';
+import Home from './pages/home';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/chat' component={Chat} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
